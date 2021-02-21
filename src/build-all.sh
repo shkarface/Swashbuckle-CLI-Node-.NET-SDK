501d7543a3793b0e126dd056938c6c27eee6c80f
Shkar T. Noori <shkarface@hotmail.com>
2021-02-21 10:13:16 +0300

    Add script to build all images

---
+++ src/build-all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DIR="`dirname \"$0\"`"
+PUSH=
+REG=shkarface/swashbuckle-cli-node-dotnet
+
+usage()
+{
+    echo "Usage: build-all.sh [options]"
+    echo
+    echo "Builds all the docker images and optionally pushes them."
+    echo
+    echo "Options:"
+    echo "    -r --registry     Repository (Default: shkarface/swashbuckle-cli-node-dotnet)"
+    echo "    --push            Push the docker image after successful build"
+    exit -1
+}
+
+while test $# -gt 0
+do
+    case $1 in
+        --push )        shift
+                        PUSH=--push
+                        ;;
+        -r | --registry ) shift
+                        REG=$1
+                        ;;
+        -h | --help )   usage
+                        exit
+                        ;;
+    esac
+    shift
+done
+
+"$DIR"/build.sh -r "$REG" --net 3.1 --node 14.x --swashbuckle 5.6.3 "$PUSH"
+echo
+
+"$DIR"/build.sh -r "$REG" --net 3.1 --node 14.x --swashbuckle 6.0.2 "$PUSH"
+echo
+
+"$DIR"/build.sh -r "$REG" --net 5.0 --node 14.x --swashbuckle 6.0.2 "$PUSH"
